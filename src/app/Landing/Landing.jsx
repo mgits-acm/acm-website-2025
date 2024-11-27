fdb40cade2f3599bcd741db01e12f5b428efef18
Jyothish <117960824+Luf1o@users.noreply.github.com>
2024-11-27 21:16:46 +0530

    way past initial

---
+++ src/app/Landing/Landing.jsx
@@ -0,0 +1,11 @@
+export default function Landing(){
+    return(
+        <div className="bg-emerald-600  md:p-0 p-3 flex w-screen h-[60vh] md:h-[90vh]">
+           <div className="w-full bg-emerald-800 text-white rounded-[30px] md:rounded-t-none h-full">
+                <h1>MITS ACM</h1>
+                <p>ACM Students chapter</p>
+                <h1>Image Tag needs work around</h1>
+           </div>
+        </div>
+    )
+}
---
+++ src/app/components/About/About.jsx
@@ -0,0 +1,18 @@
+export default function AboutSection(){
+    return(
+        <main className="w-screen h-max">
+            <div className="w-full p-5 mb-3  md:p-10 flex items-center justify-center">
+                {/**Buffer?? */}
+                <div className="w-11/12 bg-slate-600 rounded-lg sm:p-[2px] p-[1px] opacity-70"></div>
+            </div>
+            <div className="gap-10 p-5 flex items-center justify-center flex-col">
+                <h1 className="uppercase font-black text-4xl">
+                    About Us
+                </h1>
+                <p className="w-10/12 md:w-4/6 text-justify md:text-center font-light text-lg md:text-2xl">
+                    Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in  laying out print, graphic or web designs. The passage is attributed to  an unknown typesetter in the 15th century who is thought to have  scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a  type specimen book. It usually begins with:
+                </p>
+            </div>
+        </main>
+    )
+}
